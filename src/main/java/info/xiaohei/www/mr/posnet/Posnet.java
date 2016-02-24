35d6e82fa2ef8af09885e01c4d6a26af3db302c7
chubbyjiang <chubbyjiang@gmail.com>
2016-02-24 12:19:50 +0800

    根据参数选择reducer

--- src/main/java/info/xiaohei/www/mr/posnet/Posnet.java
+++ src/main/java/info/xiaohei/www/mr/posnet/Posnet.java
@@ -12,17 +12,24 @@ import java.io.IOException;
  */
 public class Posnet {
     public static void main(String[] args) throws InterruptedException, IOException, ClassNotFoundException {
-        if (args.length != 2) {
+        if (args.length < 3) {
             System.err.println("");
-            System.err.println("Usage: Posnet <date> <timepoint>");
-            System.err.println("Example: Posnet 2016-02-21 09-18-24");
+            System.err.println("Usage: Posnet <date> <timepoint> <isTotal>");
+            System.err.println("Example: Posnet 2016-02-21 09-18-24 1");
             System.exit(-1);
         }
         Configuration conf = new Configuration();
         conf.set("date", args[0]);
         conf.set("timepoint", args[1]);
-        BaseDriver.InitJob("hdfs://localhost:9000/data/2-posnet", "hdfs://localhost:9000/out/2-posnet"
-                , conf, "posnet", Posnet.class, Mapper.class, Text.class, Text.class, Reducer.class
-                , NullWritable.class, Text.class);
+        //使用统计最长停留时间的reducer
+        if (args[2].equals("1")) {
+            BaseDriver.InitJob("hdfs://localhost:9000/data/2-posnet", "hdfs://localhost:9000/out/2-posnet"
+                    , conf, "posnet", Posnet.class, Mapper.class, Text.class, Text.class, TotalReducer.class
+                    , NullWritable.class, Text.class);
+        } else {
+            BaseDriver.InitJob("hdfs://localhost:9000/data/2-posnet", "hdfs://localhost:9000/out/2-posnet"
+                    , conf, "posnet", Posnet.class, Mapper.class, Text.class, Text.class, Reducer.class
+                    , NullWritable.class, Text.class);
+        }
     }
 }
