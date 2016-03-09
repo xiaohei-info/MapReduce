2d3b02bf569c3a070dfe00974e9b8f6bca793c31
chubbyjiang <chubbyjiang@gmail.com>
2016-03-09 15:04:50 +0800

    HDFS操作类

--- src/main/java/info/xiaohei/www/HdfsUtil.java
+++ src/main/java/info/xiaohei/www/HdfsUtil.java
@@ -1,7 +1,135 @@
 package info.xiaohei.www;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.io.IOUtils;
+
+import java.io.IOException;
+import java.net.URI;
+
 /**
  * Created by xiaohei on 16/3/9.
+ * HDFS操作类
  */
 public class HdfsUtil {
+
+    //默认的hdfs url
+    private static final String HDFS = "hdfs://localhost:9000/";
+    private String hdfsPath;
+    private Configuration conf;
+
+    public HdfsUtil(Configuration conf) {
+        this(HDFS, conf);
+    }
+
+    public HdfsUtil(String hdfs, Configuration conf) {
+        this.hdfsPath = hdfs;
+        this.conf = conf;
+    }
+
+    /**
+     * 创建文件夹
+     * @param folder 文件夹名
+     * */
+    public void mkdirs(String folder) throws IOException {
+        Path path = new Path(folder);
+        FileSystem fs = FileSystem.get(URI.create(hdfsPath), conf);
+        if (!fs.exists(path)) {
+            fs.mkdirs(path);
+            System.out.println("Create: " + folder);
+        }
+        fs.close();
+    }
+
+    /**
+     * 删除文件夹
+     * @param folder 文件夹名
+     * */
+    public void rmr(String folder) throws IOException {
+        Path path = new Path(folder);
+        FileSystem fs = FileSystem.get(URI.create(hdfsPath), conf);
+        fs.deleteOnExit(path);
+        System.out.println("Delete: " + folder);
+        fs.close();
+    }
+
+    /**
+     * 列出该路径的文件信息
+     * @param folder 文件夹名
+     * */
+    public void ls(String folder) throws IOException {
+        Path path = new Path(folder);
+        FileSystem fs = FileSystem.get(URI.create(hdfsPath), conf);
+        FileStatus[] list = fs.listStatus(path);
+        System.out.println("ls: " + folder);
+        System.out.println("==========================================================");
+        for (FileStatus f : list) {
+            System.out.printf("name: %s, folder: %s, size: %d\n", f.getPath(), f.isDir(), f.getLen());
+        }
+        System.out.println("==========================================================");
+        fs.close();
+    }
+
+    /**
+     * 创建文件
+     * @param file 文件名
+     * @param content 文件内容
+     * */
+    public void createFile(String file, String content) throws IOException {
+        FileSystem fs = FileSystem.get(URI.create(hdfsPath), conf);
+        byte[] buff = content.getBytes();
+        FSDataOutputStream os = null;
+        try {
+            os = fs.create(new Path(file));
+            os.write(buff, 0, buff.length);
+            System.out.println("Create: " + file);
+        } finally {
+            if (os != null)
+                os.close();
+        }
+        fs.close();
+    }
+
+    /**
+     * 复制本地文件到hdfs
+     * @param local 本地文件路径
+     * @param remote hdfs目标路径
+     * */
+    public void copyFile(String local, String remote) throws IOException {
+        FileSystem fs = FileSystem.get(URI.create(hdfsPath), conf);
+        fs.copyFromLocalFile(new Path(local), new Path(remote));
+        System.out.println("copy from: " + local + " to " + remote);
+        fs.close();
+    }
+
+    /**
+     * 从hdfs下载文件到本地
+     * @param remote hdfs文件路径
+     * @param local 本地目标路径
+     * */
+    public void download(String remote, String local) throws IOException {
+        Path path = new Path(remote);
+        FileSystem fs = FileSystem.get(URI.create(hdfsPath), conf);
+        fs.copyToLocalFile(path, new Path(local));
+        System.out.println("download: from" + remote + " to " + local);
+        fs.close();
+    }
+
+    /**
+     * 查看hdfs文件内容
+     * @param remoteFile hdfs文件路径
+     * */
+    public void cat(String remoteFile) throws IOException {
+        Path path = new Path(remoteFile);
+        FileSystem fs = FileSystem.get(URI.create(hdfsPath), conf);
+        FSDataInputStream fsdis = null;
+        System.out.println("cat: " + remoteFile);
+        try {
+            fsdis =fs.open(path);
+            IOUtils.copyBytes(fsdis, System.out, 4096, false);
+        } finally {
+            IOUtils.closeStream(fsdis);
+            fs.close();
+        }
+    }
 }
