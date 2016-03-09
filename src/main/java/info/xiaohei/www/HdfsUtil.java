dbea545ea97b7561654c55c965b53423417918b4
chubbyjiang <chubbyjiang@gmail.com>
2016-03-09 15:04:15 +0800

    原始数据转邻接概率矩阵

---
+++ src/main/java/info/xiaohei/www/HdfsUtil.java
@@ -0,0 +1,7 @@
+package info.xiaohei.www;
+
+/**
+ * Created by xiaohei on 16/3/9.
+ */
+public class HdfsUtil {
+}
---
+++ src/main/java/info/xiaohei/www/mr/peoplerank/CalcPeopleRank.java
@@ -0,0 +1,7 @@
+package info.xiaohei.www.mr.peoplerank;
+
+/**
+ * Created by xiaohei on 16/3/9.
+ */
+public class CalcPeopleRank {
+}
