e4ff808ec0c87c98b01bb1185dca6364e51e5a0c
chubbyjiang <chubbyjiang@gmail.com>
2016-04-08 17:05:43 +0800

    驱动程序

--- src/main/java/info/xiaohei/www/mr/weiboad/WeiboAd.java
+++ src/main/java/info/xiaohei/www/mr/weiboad/WeiboAd.java
@@ -1,5 +1,8 @@
 package info.xiaohei.www.mr.weiboad;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 /**
  * Copyright © 2016 xiaohei, All Rights Reserved.
  * Email : chubbyjiang@gmail.com
@@ -7,4 +10,9 @@ package info.xiaohei.www.mr.weiboad;
  * Created : 16/4/8 14:36
  */
 public class WeiboAd {
+    public static void main(String[] args) throws InterruptedException, IOException, ClassNotFoundException, URISyntaxException {
+        CalcTFAndN.run();
+        CalcDF.run();
+        CalcTotal.run(g);
+    }
 }
